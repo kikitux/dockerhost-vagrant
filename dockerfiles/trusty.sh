8acf0e0891c0906daa00f2232d28ac9d2aa357d2
Alvaro Miranda <kikitux@gmail.com>
2016-01-23 12:12:15 +1300

    update

--- dockerfiles/trusty.sh
+++ dockerfiles/trusty.sh
@@ -40,12 +40,12 @@ fi
 pushd /vagrant/dockerfiles
 docker build -t trusty-webserver-php5 trusty-webserver-php5/.
 docker tag trusty-webserver-php5:latest trusty-webserver-php5:${DATE}
-docker build -t php5info-cli php5info-cli/.
-docker tag php5info-cli:latest php5info-cli:${DATE}
+docker build -t trusty-php5info-cli trusty-php5info-cli/.
+docker tag trusty-php5info-cli:latest trusty-php5info-cli:${DATE}
 popd
 
 #simple test for php5
-docker run php5info-cli:${DATE} php -v
+docker run trusty-php5info-cli:${DATE} php -v
 
 #delete <none> images
 NONE="`docker images -q --filter "dangling=true"`"
