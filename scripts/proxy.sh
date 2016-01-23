f7738121881f3694d6d3eb9b311ce6b5e8bc6cbf
Alvaro Miranda <kikitux@gmail.com>
2016-01-23 16:46:17 +1300

    update

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -6,10 +6,18 @@ which polipo || {
 
 http_proxy="http://127.0.0.1:8123" curl -sSL http://google.com &>/dev/null
 if [ $? -eq 0 ]; then
+  #proxy.env
   echo 'export http_proxy="http://127.0.0.1:8123"' > /vagrant/proxy.env
   echo 'export HTTP_PROXY="http://127.0.0.1:8123"' >> /vagrant/proxy.env
-  echo 'Defaults env_keep = "http_proxy ftp_proxy"' > /etc/sudoers.d/proxy
+  echo 'export https_proxy="http://127.0.0.1:8123"' >> /vagrant/proxy.env
+  echo 'export HTTPS_PROXY="http://127.0.0.1:8123"' >> /vagrant/proxy.env
+  #sudoers
+  echo 'Defaults env_keep = "http_proxy HTTP_PROXY https_proxy HTTPS_PROXY ftp_proxy"' > /etc/sudoers.d/proxy
+  #apt
+  echo 'Acquire::http::proxy "http://127.0.0.1:8123";' >  /etc/apt/apt.conf.d/30proxy 
+  echo 'Acquire::https::proxy "https://127.0.0.1:8123";' >> /etc/apt/apt.conf.d/30proxy 
 else
   > /vagrant/proxy.env
   > /etc/sudoers.d/proxy
+  > /etc/apt/apt.conf.d/30proxy
 fi
